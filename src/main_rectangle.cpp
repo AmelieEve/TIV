0b00781e2556a9340cbdd19e80b227a90e8a52e2
Maxime Sorel <sorel.max@gmail.com>
2022-12-20 12:43:01 +0100

    ENH: template matching with threshold now
    - added template matching with threshold, resulting in better precision
    in the analysis.
    - TODO NEXT => check if any other iteration of the same template within
      a page. For now, template matching only work once but doesn't work as
    intented if other same object in the page

--- src/main_rectangle.cpp
+++ src/main_rectangle.cpp
@@ -19,7 +19,7 @@ using namespace cv;
 
 int main (void) {
     //charge et affiche l'image (� MODIFIER) :
-    string imName = "../NicIcon/all-scans/00000.png";
+    string imName = "../NicIcon/all-scans/00001.png";
     Mat img_rgb = imread(imName);
     Mat img_gray;
     cvtColor(img_rgb, img_gray, COLOR_BGR2GRAY);
@@ -29,43 +29,59 @@ int main (void) {
     img_rgb.copyTo( img_display );
 
     Mat res;
-    double minVal; double maxVal; Point minLoc; Point maxLoc;
-    Point matchLoc, lastMatch;
+    Point matchLoc;
 
     /*** Un symbole, une image ***/
-/*    try{
-        matchTemplate(img_gray,shape_template, res,TM_CCOEFF_NORMED);
-        normalize( res, res, 0, 1, NORM_MINMAX, -1, Mat() );
-        minMaxLoc( res, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
-        matchLoc = maxLoc;
+    /* double minVal; double maxVal; Point minLoc; Point maxLoc;
+       try{
+           matchTemplate(img_gray,shape_template, res,TM_CCOEFF_NORMED);
+           normalize( res, res, 0, 1, NORM_MINMAX, -1, Mat() );
+           minMaxLoc( res, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+           matchLoc = maxLoc;
 
-        Rect crop(matchLoc.x , matchLoc.y - 120, 2100, 332);
-        Mat sub_image = img_display(crop);
-        rectangle( img_display, Point( matchLoc.x , matchLoc.y - 120 ), Point( matchLoc.x + 2100 , matchLoc.y + 212 ), Scalar::all(0), 2, 8, 0 );
-//        imshow( "image_window", img_display );
-        imshow( "triangle_sub_image", sub_image );
-    }catch(cv::Exception &exception){
+           Rect crop(matchLoc.x , matchLoc.y - 120, 2100, 332);
+           Mat sub_image = img_display(crop);
+           rectangle( img_display, Point( matchLoc.x , matchLoc.y - 120 ), Point( matchLoc.x + 2100 , matchLoc.y + 212 ), Scalar::all(0), 2, 8, 0 );
+   //        imshow( "image_window", img_display );
+           imshow( "triangle_sub_image", sub_image );
+       }catch(cv::Exception &exception){
 
-    }*/
+       }*/
 
 
     /*** Une image, tous les symboles ***/
-    vector<cv::String> sym;
-    glob("../symboles/*.png", sym, false);
+    vector<cv::String> symb;
+    glob("../symboles/*.png", symb, false);
     vector<Mat> symboles;
-    size_t count = sym.size();
+    size_t count = symb.size();
     for (size_t i=0; i<count; i++) {
-        std::cout << "symbole testé : " << sym[i] << std::endl;
-        symboles.push_back(imread(sym[i],IMREAD_GRAYSCALE));
+        std::cout << "symbole testé : " << symb[i] << std::endl;
+        symboles.push_back(imread(symb[i],IMREAD_GRAYSCALE));
         try {
-            matchTemplate(img_gray, symboles[i], res, TM_CCOEFF_NORMED);
-            normalize(res, res, 0, 1, NORM_MINMAX, -1, Mat());
-            minMaxLoc(res, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-            matchLoc = maxLoc;
-
-            Rect crop(matchLoc.x - 30, matchLoc.y - 120, 2100, 332);
-            Mat sub_image = img_display(crop);
-            imshow(  "test " + sym[i], sub_image);
+            matchTemplate(img_gray, symboles[i], res, TM_CCORR_NORMED);
+            threshold(res, res, 0.1, 1., THRESH_TOZERO);
+            while(true)
+            {
+                double minval, maxval;
+                double threshold = (i == 4? 0.995 : 0.99); //Pour le symbole electricity (i=4), il reconnait parfois la croix en bas comme étant le symbole electricity donc augmenter le seuil pour i=4
+                Point minloc, maxloc;
+                minMaxLoc(res, &minval, &maxval, &minloc, &maxloc);
+                matchLoc = maxloc;
+                
+                if(maxval >= threshold)
+                {
+                    Rect crop(matchLoc.x - 40, matchLoc.y - 120, 2100, 360);
+                    Mat sub_image = img_display(crop);
+                    imshow(  "test " + symb[i], sub_image);
+//                    i--; //pour retester le meme objet si on l'a déjà trouvé, mais il faut dans ce cas que je genere une sous image ne contenant pas la partie extraite puis refaire l'opération dessus //TODO
+                    break;
+                }
+                else
+                {
+                    cout << "\nTemplate does not match with input image\n";
+                    break;
+                }
+            }
         }catch(cv::Exception &exception){
             std::cerr << "symbole N°" << i << " not found" << std::endl;
         }
